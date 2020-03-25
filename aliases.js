824935fb48bba031bddaee1791d5f9304a7690b8
Travis CI User <travis@example.org>
2020-03-25 06:07:33 +0000

    Update documentation

---
+++ aliases.js
@@ -0,0 +1,8 @@
+var ALIASES = {};
+ALIASES["as_slice"] = {};
+ALIASES["byteorder"] = {};
+ALIASES["generic_array"] = {};
+ALIASES["hash32"] = {};
+ALIASES["heapless"] = {};
+ALIASES["stable_deref_trait"] = {};
+ALIASES["typenum"] = {};
---
+++ implementors/stable_deref_trait/trait.StableDeref.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["heapless"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"stable_deref_trait/trait.StableDeref.html\" title=\"trait stable_deref_trait::StableDeref\">StableDeref</a> for <a class=\"struct\" href=\"heapless/pool/struct.Box.html\" title=\"struct heapless::pool::Box\">Box</a>&lt;T&gt;","synthetic":false,"types":["heapless::pool::Box"]}];
+implementors["stable_deref_trait"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
